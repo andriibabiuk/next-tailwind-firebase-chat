61e714cb816132d32b38ca7a4e7fe4d425a3084c
andriibabiuk <andrii.babiuk.dev@gmail.com>
2026-07-08 14:20:17 +0300

    feat: add base structure for home page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,38 @@
+'use client';
+import { app, firestore } from '@/lib/firebase';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { doc, getDoc } from 'firebase/firestore';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import ChatRoom from './components/ChatRoom';
+import Users from './components/Users';
+
 export default function Home() {
+	const auth = getAuth(app);
+	const [user, setUser] = useState(null);
+	const router = useRouter();
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, async user => {
+			if (user) {
+				const userRef = doc(firestore, 'users', user.uid);
+				const userSnap = await getDoc(userRef);
+				const userData = userSnap.data();
+				setUser(userData);
+			} else {
+				setUser(null);
+				router.push('/login');
+			}
+		});
+		return () => unsubscribe();
+	}, [auth, router]);
 	return (
-		<div>
-			<button className='btn btn-primary'>Button</button>
+		<div className='flex h-screen'>
+			<div className='shrink-0 w-3/12'>
+				<Users />
+			</div>
+			<div className='grow w-3/12'>
+				<ChatRoom />
+			</div>
 		</div>
 	);
 }
